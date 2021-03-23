55392560601b5d569adf8b66b437ecff623dba75
Janliz Suarez <jsuarezrosado@gmail.com>
2021-03-22 21:59:07 -0500

    added content on all pages

--- src/components/pages/ContactMe/ContactMe.js
+++ src/components/pages/ContactMe/ContactMe.js
@@ -1,9 +1,31 @@
 import React from "react";
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
-function ContactMe(){
-    return( <h1>
-        Contact Me
-    </h1>)
+
+function ContactMe() {
+    return (
+        <div>
+
+
+            <h1 className="display-4">Contact Me!</h1>
+            <p className="lead">Feel free to send me a message.</p>
+
+            <Form>
+                <Form.Group controlId="exampleForm.ControlInput1">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="name@example.com" />
+                </Form.Group>
+
+                <Form.Group controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Example textarea</Form.Label>
+                    <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+            </Form>
+            <Button type="submit">Submit form</Button>
+
+        </div>
+    )
 }
 
 export default ContactMe;
--- src/components/pages/Portfolio/Portfolio.js
+++ src/components/pages/Portfolio/Portfolio.js
@@ -1,9 +1,62 @@
 import React from "react";
+import Card from 'react-bootstrap/Card'
+import CardGroup from 'react-bootstrap/CardGroup'
+import Button from 'react-bootstrap/Button'
+import { Link } from 'react-router-dom';
 
 function Portfolio(){
-    return( <h1>
-       Portfolio
-    </h1>)
+    return( 
+       <div>
+           <div>
+               <h1>
+               Welcome to my portfolio
+               </h1>
+               <h2>
+               Working on good things. More are coming soon!
+               </h2>
+           </div>
+    <CardGroup>
+        <Card>
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            <Card.Title>Work Day Scheduler</Card.Title>
+            <Card.Text>
+            Schedule important meetings or project thur a working day. Hours will change colors if is past, present, or future.
+            </Card.Text>
+          </Card.Body>
+          <Link to="https://civicajr91.github.io/calendar/">
+          <Button variant="primary">See Project</Button>
+          </Link>
+          
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            <Card.Title>Password Generator</Card.Title>
+            <Card.Text>
+            Need helps getting secute passowords? Use my password generator. Follow the prompt steps to generate a password.{' '}
+            </Card.Text>
+          </Card.Body>
+          <Link to="https://www.linkedin.com/in/janlizsuarezrosado/">
+          <Button variant="primary">See Project</Button>
+          </Link>
+         
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            <Card.Title>Burger Logger App</Card.Title>
+            <Card.Text>
+            It's a CLI Application using Express Handlebars.All data is created and stores in MySQL.
+            </Card.Text>
+          </Card.Body>
+          <Link to="https://www.linkedin.com/in/janlizsuarezrosado/">
+          <Button variant="primary">See Project</Button>
+          </Link>
+        </Card>
+      </CardGroup>
+      </div>
+    )
 }
 
 export default Portfolio;
